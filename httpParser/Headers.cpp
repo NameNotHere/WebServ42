0f446454b40904fa9b8c342ce2d6d1d8b6c0a75f
root <root@JacksPC>
2026-08-31 12:05:56 +0200

    Header parser working

--- httpParser/Headers.cpp
+++ httpParser/Headers.cpp
@@ -6,50 +6,57 @@ bool HttpParser::headers(const std::string& request)
 	if(posStart == std::string::npos)
 		return false;
 
-	size_t posFin = request.find("\r\n", posStart + 2);
-	if(posFin == std::string::npos)
-		return false;
+	while(1)
+	{
+		size_t posFin = request.find("\r\n", posStart + 2);
+		if(posFin == std::string::npos)
+			return false;
 
-	std::string header = request.substr(posStart + 2, posFin - posStart - 2);
-	std::cout << "Header = " << header << "\n";
-	
-	size_t colon = header.find(":");
-	if(colon == std::string::npos)
-		return false;
-	
-	std::string name = header.substr(0, colon);
-	std::string value = header.substr(colon + 1);
+		std::string header = request.substr(posStart + 2, posFin - posStart - 2);
+		std::cout << "Header = " << header << "\n";
+		
+		size_t colon = header.find(":");
+		if(colon == std::string::npos)
+			return false;
+		
+		std::string name = header.substr(0, colon);
+		std::string value = header.substr(colon + 1);
 
-	if(!validChar(name))
-		return false;
+		if(!validChar(name))
+			return false;
 
-	// Trim white space from both ends
-	size_t start = value.find_first_not_of(" \t");
-	if(start != std::string::npos)
-		value = value.substr(start);
-	std::cout << "Value after start trim = " << value << "\n";
+		// Trim white space from both ends
+		size_t start = value.find_first_not_of(" \t");
+		if(start != std::string::npos)
+			value = value.substr(start);
+		std::cout << "Value after start trim = " << value << "\n";
 
-	size_t end = value.find_last_not_of(" \t");
-	if(end != std::string::npos)
-		value = value.substr(0, end + 1);
-	std::cout << "Value after end trim = " << value << "\n";
+		size_t end = value.find_last_not_of(" \t");
+		if(end != std::string::npos)
+			value = value.substr(0, end + 1);
+		std::cout << "Value after end trim = " << value << "\n";
 
-	// Check for white space within value
-	// size_t spaces = value.find(" ");
-	// if(spaces != std::string::npos)
-	// 	return false;
-	// spaces = value.find("\t");
-	// if(spaces != std::string::npos)
-	// 	return false;
-	
-	if(value.find("\n") != std::string::npos || value.find("\r") != std::string::npos)
-		return false;
+		// Check for white space within value
+		// size_t spaces = value.find(" ");
+		// if(spaces != std::string::npos)
+		// 	return false;
+		// spaces = value.find("\t");
+		// if(spaces != std::string::npos)
+		// 	return false;
+		
+		if(value.find("\n") != std::string::npos || value.find("\r") != std::string::npos)
+			return false;
 
-	lowerCase(name);
-	_headers[name] = value;
+		lowerCase(name);
+		_headers[name] = value;
 
-	std::cout << "Name = " << name << "\n";
-	std::cout << "Value = " << value << "\n";
+		std::cout << "Name = " << name << "\n";
+		std::cout << "Value = " << value << "\n\n";
+
+		posStart = posFin;
+		if(request[posStart + 2] == '\r' && request[posStart + 3] == '\n')
+			break;
+	}
 
 	return true;
 }
--- httpParser/HttpTester.cpp
+++ httpParser/HttpTester.cpp
@@ -6,7 +6,7 @@ int main(void)
 {
 	HttpParser test;
 
-	test.parseHttpRequest("GET /index.html HTTP/1.1\r\nHoSt:  	loCalhOst:8080			\r\n");
+	test.parseHttpRequest("GET /index.html HTTP/1.1\r\nHoSt: loCalhOst:8080  \r\nConnection:    keep-alive	\r\nUser-Agent: 	Mozilla/5.0		\r\nContent-Length: 42\r\n\r\n");
 
 	return 0;
 }
