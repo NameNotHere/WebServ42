d41db8661952218424775149a35b44309d5f15d8
root <root@JacksPC>
2026-09-16 14:00:28 +0200

    Process requests

---
+++ ProcessRequests/Post.hpp
@@ -0,0 +1,8 @@
+#ifndef POST_HPP
+#define POST_HPP
+
+#include "../httpParser/HttpParser.hpp"
+
+
+
+#endif
--- test.sh
+++ test.sh
@@ -7,23 +7,18 @@ run_test()
 {
     echo
     echo "===== $1 ====="
-	printf "%b" "$2" | nc -q 0 "$HOST" "$PORT"    echo
+    printf "%b" "$2" | nc -q 0 "$HOST" "$PORT"
+    echo
 }
 
-run_test "1. Valid chunked request" \
-'POST / HTTP/1.1\r\nHost: localhost:2020\r\nTransfer-Encoding: chunked\r\n\r\n4\r\ntest\r\n0\r\n\r\n'
+run_test "1. Multiple chunks" \
+'POST / HTTP/1.1\r\nHost: localhost:2020\r\nTransfer-Encoding: chunked\r\n\r\n3\r\nabc\r\n3\r\ndef\r\n0\r\n\r\n'
 
-run_test "2. Valid hexadecimal chunk size" \
-'POST / HTTP/1.1\r\nHost: localhost:2020\r\nTransfer-Encoding: chunked\r\n\r\nA\r\n1234567890\r\n0\r\n\r\n'
+run_test "2. Empty chunked body" \
+'POST / HTTP/1.1\r\nHost: localhost:2020\r\nTransfer-Encoding: chunked\r\n\r\n0\r\n\r\n'
 
-run_test "3. Invalid hex character" \
-'POST / HTTP/1.1\r\nHost: localhost:2020\r\nTransfer-Encoding: chunked\r\n\r\nG\r\ntest\r\n0\r\n\r\n'
+run_test "3. Chunk extension" \
+'POST / HTTP/1.1\r\nHost: localhost:2020\r\nTransfer-Encoding: chunked\r\n\r\n0;foo=bar\r\n\r\n'
 
-run_test "4. Incomplete chunk" \
-'POST / HTTP/1.1\r\nHost: localhost:2020\r\nTransfer-Encoding: chunked\r\n\r\n6\r\ntest'
-
-run_test "5. Missing final CRLF" \
-'POST / HTTP/1.1\r\nHost: localhost:2020\r\nTransfer-Encoding: chunked\r\n\r\n4\r\ntest\r\n0\r\n'
-
-run_test "6. Data after zero chunk" \
-'POST / HTTP/1.1\r\nHost: localhost:2020\r\nTransfer-Encoding: chunked\r\n\r\n4\r\ntest\r\n0\r\nhello\r\n'
+run_test "4. Chunked + second request" \
+'POST /first HTTP/1.1\r\nHost: localhost:2020\r\nTransfer-Encoding: chunked\r\n\r\n3\r\nabc\r\n0\r\n\r\nGET /second HTTP/1.1\r\nHost: localhost:2020\r\n\r\n'
